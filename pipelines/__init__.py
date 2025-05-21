d97ce05f014a95a833714b0d529e69905774c437
Shanilka Haturusinghe <s.haturusinghe99@gmail.com>
2025-05-21 08:56:01 +0000

    Add end_to_end_data to pipeline exports

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,8 +1,11 @@
 from .digital_data_etl import digital_data_etl
+from .end_to_end_data import end_to_end_data
 from .feature_engineering import feature_engineering
-
+from .generate_datasets import generate_datasets
 
 __all__ = [
+    "generate_datasets",
+    "end_to_end_data",
     "digital_data_etl",
-    "feature_engineering"
+    "feature_engineering",
 ]
