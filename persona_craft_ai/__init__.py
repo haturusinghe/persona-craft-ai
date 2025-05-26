06f0d4a98e8ac926317b0a3cf0b4162d5365781a
Shanilka Haturusinghe <s.haturusinghe99@gmail.com>
2025-05-26 10:11:53 +0000

    Add 'model' to imports and update __all__ export in __init__.py

--- persona_craft_ai/__init__.py
+++ persona_craft_ai/__init__.py
@@ -1,4 +1,4 @@
-from persona_craft_ai import application, domain, infrastructure
+from persona_craft_ai import application, domain, infrastructure, model
 from persona_craft_ai.settings import settings
 
-__all__ = ["settings", "application", "domain", "infrastructure"]
+__all__ = ["settings", "application", "domain", "infrastructure", "model"]
